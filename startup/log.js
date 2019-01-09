f0210b6c71af480b72ead999d26b2c9ed83ac6ac
shimakee <deleon.services@outlook.com>
2019-01-09 12:33:03 +0800

    remove winston connecting to database for logging, unknown error

--- startup/log.js
+++ startup/log.js
@@ -3,7 +3,7 @@ const winston = require('winston'); //error handler
 require('winston-mongodb');//-*
 require('express-async-errors');//route try catch wrapper
 
-const URI_ERROR = config.get('db_log.uri');//database//-*
+// const URI_ERROR = config.get('db_log.uri');//database//-*
 
 module.exports = function(){
     if(config.util.getEnv('NODE_ENV') === "test"){
@@ -22,5 +22,5 @@ module.exports = function(){
         throw err;//pass to uncaughtExceptions
     });
     winston.add(winston.transports.File, {filename: 'appLogFile.log'});//create log file
-    winston.add(winston.transports.MongoDB, {db: URI_ERROR}); //database for errors//-*
+    // winston.add(winston.transports.MongoDB, {db: URI_ERROR}); //database for errors//-*
 }
