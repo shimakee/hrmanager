71986baf19857d18b8f14699e79eec34d67a33bd
shimakee <deleon.services@outlook.com>
2019-01-10 10:20:05 +0800

    changed cors option origin included localhost:8080

--- startup/util.js
+++ startup/util.js
@@ -9,20 +9,21 @@ module.exports = function(app){
     //CORS
     app.options('*', cors()); //enable pre-flight
     //cors options
-    const corsWhitelist = ['http://120.28.193.241', 'http://49.148.197.201', 'https://deleonhr.herokuapp.com', 'http://localhost', 'https://localhost'];// whitelist sources
+    const corsWhitelist = ['http://120.28.193.241', 'http://49.148.197.201', 'https://deleonhr.herokuapp.com', 'http://localhost', 'https://localhost', 'http://localhost:8080', 'https://localhost:8080'];// whitelist sources
     const corsOption = {
-            origin: function(origin, callback){
-                //     console.log('origin', origin);
-                    if(corsWhitelist.indexOf(origin) !== -1 || !origin){
-                            callback(null, true);
-                    }else{
-                            callback(new Error('Source not allowed by cors.'));
-                    }
-            },
-            methods: ['GET','POST','PUT','DELETE'],
-            exposeHeaders:[config.get('token_header'), 'Content-Type', 'X-Auth-Hureon', 'Set-Cookie', 'Authorization'],
-            allowHeaders:[config.get('token_header'), 'Content-Type', 'X-Auth-Hureon', 'Set-Cookie', 'Authorization'],
-            preflightContinue: true
+        origin: function(origin, callback){
+        //     console.log('origin', origin);
+                if(corsWhitelist.indexOf(origin) !== -1 || !origin){
+                        callback(null, true);
+                }else{
+                        callback(new Error('Source not allowed by cors.'));
+                }
+        },
+        // origin: '*',
+        methods: ['GET','POST','PUT','DELETE'],
+        exposeHeaders:[config.get('token_header'), 'Content-Type', 'X-Auth-Hureon', 'Set-Cookie', 'Authorization'],
+        allowHeaders:[config.get('token_header'), 'Content-Type', 'X-Auth-Hureon', 'Set-Cookie', 'Authorization'],
+        preflightContinue: true
     }
     //implement cors
     app.use(cors(corsOption)); //allow cors
